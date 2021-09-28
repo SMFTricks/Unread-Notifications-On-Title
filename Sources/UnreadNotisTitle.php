8de57e3b792c7c71e373d318af2d89fe74ea4396
Diego Andrés <diegoandres_cortes@outlook.com>
2021-09-28 00:09:53 -0500

    v1.1
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Sources/UnreadNotisTitle.php
+++ Sources/UnreadNotisTitle.php
@@ -2,7 +2,7 @@
 
 /**
  * @package Unread Notifications On Title
- * @version 1.0
+ * @version 1.1
  * @author Diego Andrés <diegoandres_cortes@outlook.com>
  * @copyright Copyright (c) 2020, Diego Andrés
  * @license https://www.mozilla.org/en-US/MPL/2.0/
@@ -13,12 +13,63 @@ if (!defined('SMF'))
 
 class UnreadNotisTitle
 {
-	public static function theme_context()
+	/**
+	 * @var int The noti count
+	 */
+	private $_notifications;
+
+	/**
+	 * UnreadNotisTitle::__construct()
+	 *
+	 *  Initialize the notis to 0 and then start adding them
+	 * 
+	 * @return void
+	 */
+	public function __construct()
+	{
+		// No notis
+		$this->_notifications = 0;
+
+		// Add the notis
+		$this->find_notis();
+	}
+
+	/**
+	 * UnreadNotisTitle::theme_context()
+	 *
+	 *  Insert the notis in the title
+	 * 
+	 * @return void
+	 */
+	public function theme_context()
 	{
 		global $context;
 
-		// Alerts or PM's
-		if ((!empty($context['user']['unread_messages']) || !empty($context['user']['alerts'])) && !$context['user']['is_guest'])
-			$context['page_title_html_safe'] = '(' . ($context['user']['unread_messages'] + $context['user']['alerts']) . ') ' . $context['page_title_html_safe'];
+		// Insert the notis in the title
+		if (!empty($this->_notifications))
+			$context['page_title_html_safe'] = '(' . ($this->_notifications) . ') ' . $context['page_title_html_safe'];
+	}
+
+	/**
+	 * UnreadNotisTitle::find_notis()
+	 *
+	 *  Load and add the different notis
+	 * Allow other mods to hook their noti count too
+	 * 
+	 * @return void
+	 */
+	public function find_notis()
+	{
+		global $context;
+
+		// Alerts
+		$this->_notifications += $context['user']['alerts'];
+		// Personal Messages
+		$this->_notifications += $context['user']['unread_messages'];
+		// Mod Actions
+		$this->_notifications += $context['open_mod_reports'];
+
+		// Allow other mods to add their notis
+		call_integration_hook('integrate_mod_unreadnotistitle', [&$this->_notifications]);
 	}
 }
